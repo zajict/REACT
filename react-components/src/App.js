6c697a4d3d3beb82dc9110b72f031ed4a89635e4
Tijana Zajic <tijanazajic1204@gmail.com>
2023-03-14 20:07:36 +0100

    react elemnts

---
+++ react-components/src/App.js
@@ -0,0 +1,54 @@
+/* eslint-disable jsx-a11y/heading-has-content */
+/* eslint-disable react/jsx-no-undef */
+import './App.css';
+
+function App() {
+  return (
+    <div className="App BIT-SHOW">
+        <Navigation>
+          <logo></logo>
+          <form>
+            <input>
+            </input>
+          </form>
+        </Navigation>
+        <main>
+          <image></image>
+          <h3></h3>
+          <p></p>
+          <sideList>
+            <ul>
+              <li>
+                <text></text>
+              </li>
+            </ul>
+          </sideList>
+          <h3></h3>
+          <ul>
+            <li>
+              <text></text>
+            </li>
+          </ul>
+             <h3></h3>
+          <ul>
+            <li>
+              <text></text>
+            </li>
+          </ul>
+             <h3></h3>
+          <ul>
+            <li>
+              <text></text>
+            </li>
+          </ul>
+        </main>
+      <footer>
+        <p></p>
+        <copy></copy>
+        <p></p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-elments/src/App.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./App.css";
+
+export default function App() {
+    return (
+        <div className="Main-content">
+            <div className="first-unordered-list">
+                <ul>
+                    <li>1 cup Fresh Basil</li>
+                    <li>1 cup Pine Nuts</li>
+                    <li>2 cups Butter Lettuce</li>
+                    <li>1 Yellow Squash</li>
+                    <li>1/2 cup Olive Oil</li>
+                    <li>3 cloves of Garlic</li>
+                </ul>
+            </div>
+            <div className="second-ordered-list">
+                <ol>
+                     <li>French onion soup</li>
+                    <li>Pizza</li>
+                    <li>croissants</li>
+                    <li>Meat balls</li>
+                    <li>Beef with Garlic sauce</li>
+                    <li>Spaghetti</li>
+                </ol>
+            </div>
+            <div>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7thSMq5t8G94bZi_Czg5OtcvcdarFQLT68Q&usqp=CAU" alt="Spaghetti"></img>
+            </div>
+        </div>
+
+     
+        
+        
+        
+    )
+}
+
